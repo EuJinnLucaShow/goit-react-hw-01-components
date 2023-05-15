d7173ce968c3b1262f1f83554fc1371cbea3215b
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-15 13:01:31 +0300

    data folder

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
-import user from './Profile/user.json';
+import user from '../data/user.json';
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import data from './Statistics/data.json';
+import data from '../data/data.json';
 import FriendList from './FriendList/FriendList';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
+import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
 
 
