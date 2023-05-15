a0cac7d512f2f8f1a30b5e09f271f5f9ecaa8e13
Yevhenii Lukashov <eujinnlucashow@gmail.com>
2023-05-15 13:46:24 +0300

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,11 @@
-import user from '../data/user.json';
 import Profile from './Profile/Profile';
+import user from '../data/user.json';
 import Statistics from './Statistics/Statistics';
 import data from '../data/data.json';
 import FriendList from './FriendList/FriendList';
 import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
 import TransactionHistory from './TransactionHistory/TransactionHistory';
-
+import transactions from '../data/transactions.json';
 
 export const App = () => {
   return ( 
@@ -16,8 +15,7 @@ export const App = () => {
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
-        stats={user.stats}
-    /> 
+        stats={user.stats} /> 
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
       <TransactionHistory items={transactions} />
